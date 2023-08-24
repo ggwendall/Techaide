9f3bdd08a75f43ba0112a40f8f924ae64bc1cb51
Gwendal <48108275+ggwendall@users.noreply.github.com>
2023-08-24 10:59:09 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -45,11 +45,45 @@ def submit_donation():
             cursor.close()
             connection.close()
 
-            # Rediriger vers une page de confirmation ou d'accusé de réception
-            return "Merci pour votre donation !"
+            # Afficher un message de remerciement avec les styles spécifiques
+            return """
+            
+                <html>
+                <head>
+                    <link rel="stylesheet" href="static\css\submit-donation.css">
+                </head>
+                <body class="submit-donation-page">
+                    <div class="thank-you-message">
+                        <h2>Merci pour votre donation !</h2>
+                        <p>Nous vous sommes très reconnaissants pour votre générosité.</p>
+                    </div>
+                </body>
+                </html>
+            """
 
         except Exception as e:
             return f"Une erreur est survenue : {e}"
+
+
+@app.route("/donate_list", methods=["GET"])
+def donate_list():
+    try:
+        connection = mysql.connector.connect(**db_config)
+        cursor = connection.cursor()
+
+        # Exécutez votre requête SQL pour récupérer la liste des dons
+        query = "SELECT * FROM donations"
+        cursor.execute(query)
+        donations = cursor.fetchall()
+
+        cursor.close()
+        connection.close()
+
+        return render_template("donate_list.html", donations=donations)
+
+    except Exception as e:
+        return f"Une erreur est survenue : {e}"
+
 
 if __name__ == "__main__":
     app.run()
